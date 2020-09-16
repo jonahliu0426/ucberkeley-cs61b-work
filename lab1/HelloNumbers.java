2d4464918f8854b66e2c641dce088749fd61257c
JiongnanLiu <jia7833@hotmail.com>
2020-09-16 14:31:26 -0400

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,9 @@
+public class HelloNumbers {
+	public static void main(String[] args){
+		int i = 0;
+		while (i < 10){
+			System.out.print(i + " ");
+			i += 1;
+		}
+	}
+}
---
+++ lab1/HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld {
+		public static void main(String[] args) {
+		
+		System.out.println("The value of 1+1 is " + (1+1));
+		int a = 10 + 5;
+		System.out.println("The value of a is " + 15);
+		int b = 50 - 23;
+		System.out.println("The value of b is " + 27);
+		int c = 12 * 13;
+		System.out.println("The value of c is " + 156);
+		double d = 20.0 / 3.0;
+		System.out.println("The value of d is " + 6.666666666666667);
+		int e = 100 % 7;
+		System.out.println("The value of e is " + 2);
+		double f = Math.pow(4, 3);
+		System.out.println("The value of f is " + 64.0);
+	}
+}
